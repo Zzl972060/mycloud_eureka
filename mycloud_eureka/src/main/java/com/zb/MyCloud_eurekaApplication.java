2a43fea88d877f3315209acef9658a86bc280257
Zzl972060 <54828637+Zzl972060@users.noreply.github.com>
2019-09-03 14:21:06 +0800

    Update MyCloud_eurekaApplication.java

--- mycloud_eureka/src/main/java/com/zb/MyCloud_eurekaApplication.java
+++ mycloud_eureka/src/main/java/com/zb/MyCloud_eurekaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 //朱振龙
+//朱振龙01
 public class MyCloud_eurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyCloud_eurekaApplication.class,args);
