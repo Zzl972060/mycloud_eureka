99772983a84abe52b6fb06e1f919a486ab5b9dbd
zzl <863593420@qq.com>
2019-09-03 14:15:32 +0800

    update zzl

--- mycloud_eureka/src/main/java/com/zb/MyCloud_eurekaApplication.java
+++ mycloud_eureka/src/main/java/com/zb/MyCloud_eurekaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+//朱振龙
 public class MyCloud_eurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyCloud_eurekaApplication.class,args);
